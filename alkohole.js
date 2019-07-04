42b712f403af05ef9757a7c281385b93456c12ab
idkrn <37932443+idkrn@users.noreply.github.com>
2019-07-04 20:39:47 +0200

    Add files via upload

---
+++ alkohole.js
@@ -0,0 +1,406 @@
+// Nie wszystko to piwa ale co z tego
+let piwa = [
+    {
+        part: "Har",
+        part2: "naś",
+        piwo: "Harnaś"
+    }, 
+    {
+        part:"Tys",
+        part2: "kie",
+        piwo: "Tyskie"
+    },
+    {
+        part: "Hal",
+        part2: "ne", 
+        piwo: "Halne"
+    },
+    {
+        part: "Dęb",
+        part2: "owe",
+        piwo: "Dębowe"
+    },
+    {
+        part: "Ży",
+        part2: "wiec",
+        piwo: "Żywiec"
+    },
+    {
+        part: "Tat",
+        part2: "ra",
+        piwo: "Tatra"
+    },
+    {
+        part: "Kus",
+        part2: "tosz",
+        piwo: "Kustosz"
+    },
+    {
+        part: "Bal",
+        part2: "on",
+        piwo: "Balon"
+    },
+    {
+        part: "Pie",
+        part2: "run",
+        piwo: "Pierun"
+    },
+    {
+        part: "Amar",
+        part2: "ena",
+        piwo: "Amarena"
+    },
+    {
+        part: "Szlach",
+        part2: "eckie",
+        piwo: "Szlacheckie"
+    },
+    {
+        part: "Karp",
+        part2: "ackie",
+        piwo: "Karpackie"
+    },
+    {
+        part: "Śląs",
+        part2: "kie",
+        piwo: "Śląskie"
+    },
+    {
+        part: "Rast",
+        part2: "inger",
+        piwo: "Rastinger"
+    },
+    {
+        part: "Fas",
+        part2: "berg",
+        piwo: "Fasberg"
+    },
+    {
+        part: "Leż",
+        part2: "ajsk",
+        piwo: "Leżajsk"
+    },
+    {
+        part: "Lech",
+        part2: "lech",
+        piwo: "Lech"
+    },
+    {
+        part: "Becz",
+        part2: "kowe",
+        piwo: "Beczkowe"
+    },
+    {
+        part: "Książ",
+        part2: "ęce",
+        piwo: "Książęce"
+    },
+    {
+        part: "Wa",
+        part2: "rka",
+        piwo: "Warka"
+    },
+    {
+        part: "Den",
+        part2: "aturat",
+        piwo: "Denaturat"
+    },
+    {
+        part: "Spir",
+        part2: "ytus",
+        piwo: "Spirytus"
+    },
+    {
+        part: "Żubr",
+        part2: "ówka",
+        piwo: "Żubrówka"
+    },
+    {
+        part: "Żubr",
+        part2: "żubr",
+        piwo: "Żubr"
+    },
+    {
+        part: "Despe",
+        part2: "rados",
+        piwo: "Desperados"
+    },
+    {
+        part: "Carls",
+        part2: "berg",
+        piwo: "Carlsberg"
+    },
+    {
+        part: "Sommer",
+        part2: "sby",
+        piwo: "Sommersby"
+    },
+    {
+        part: "Strzel",
+        part2: "ec",
+        piwo: "Strzelec"
+    },
+    {
+        part: "Alas",
+        part2: "ka",
+        piwo: "Alaska"
+    },
+    {
+        part: "Park",
+        part2: "owa",
+        piwo: "Parkowa"
+    },
+    {
+        part: "Żyt",
+        part2: "niówka",
+        piwo: "Żytniówka"
+    },
+    {
+        part: "Sop",
+        part2: "lica",
+        piwo: "Soplica"
+    },
+    {
+        part: "Żołąd",
+        part2: "kowa",
+        piwo: "Żołądkowa"
+    },
+    {
+        part: "Barm",
+        part2: "ańska",
+        piwo: "Barmańska"
+    },
+    {
+        part: "Snaj",
+        part2: "per",
+        piwo: "Snajper"
+    },
+    {
+        part: "Faj",
+        part2: "rant",
+        piwo: "Fajrant"
+    },
+    {
+        part: "Koman",
+        part2: "dos",
+        piwo: "Komandos"
+    },
+    {
+        part: "Pat",
+        part2: "yk",
+        piwo: "Patyk"
+    },
+    {
+        part: "Che",
+        part2: "rry",
+        piwo: "Cherry"
+    },
+    {
+        part: "Oko",
+        part2: "cim",
+        piwo: "Okocim"
+    },
+    {
+        part: "Don",
+        part2: "ner",
+        piwo: "Donner"
+    },
+    {
+        part: "Mich",
+        part2: "el",
+        piwo: "Michel"
+    },
+    {
+        part: "Igris",
+        part2: "toje",
+        piwo: "Igristoje"
+    },
+    {
+        part: "Sang",
+        part2: "rinita",
+        piwo: "Sangrinita"
+    },
+    {
+        part: "Boc",
+        part2: "ian",
+        piwo: "bocian"
+    },
+    {
+        part: "Fin",
+        part2: "landia",
+        piwo: "Finlandia"
+    },
+    {
+        part: "Per",
+        part2: "ła",
+        piwo: "Perła"
+    },
+    {
+        part: "Kaszt",
+        part2: "elan",
+        piwo: "Kasztelan"
+    },
+    {
+        part: "Spec",
+        part2: "jal",
+        piwo: "Specjal"
+    },
+    {
+        part: "Lab",
+        part2: "el",
+        piwo: "Label"
+    },
+    {
+        part: "Miło",
+        part2: "sław",
+        piwo: "Miłosław"
+    },
+    {
+        part: "Namy",
+        part2: "słów",
+        piwo: "Namysłów"
+    },
+    {
+        part: "Fox",
+        part2: "fox",
+        piwo: "Fox"
+    },
+    {
+        part: "Romp",
+        part2: "er",
+        piwo: "Romper"
+    },
+    {
+        part: "Kufl",
+        part2: "owe",
+        piwo: "Kuflowe"
+    },
+    {
+        part: "St",
+        part2: "ern",
+        piwo: "Stern"
+    },
+    {
+        part: "Ballan",
+        part2: "tines",
+        piwo: "Ballantines"
+    },
+    {
+        part: "Keler",
+        part2: "is",
+        piwo: "Keleris"
+    },
+    {
+        part: "Woj",
+        part2: "ak",
+        piwo: "Wojak"
+    },
+    {
+        part: "Bier",
+        part2: "hof",
+        piwo: "Bierhof"
+    },
+    {
+        part: "Amb",
+        part2: "er",
+        piwo: "Amber"
+    },
+    {
+        part: "Choj",
+        part2: "rak",
+        piwo: "Chojrak"
+    },
+    {
+        part: "Lubel",
+        part2: "ska",
+        piwo: "Lubelska"
+    },
+    {
+        part: "Kozac",
+        part2: "kie",
+        piwo: "Kozackie"
+    },
+    {
+        part: "Ba",
+        part2: "ca",
+        piwo: "Baca"
+    },
+    {
+        part: "Brew",
+        part2: "er",
+        piwo: "Brewer"
+    },
+    {
+        part: "Kadar",
+        part2: "ka",
+        piwo: "Kadarka"
+    },
+    {
+        part: "Pra",
+        part2: "żubr",
+        piwo: "Prażubr"
+    },
+    {
+        part: "Żate",
+        part2: "ckie",
+        piwo: "Żateckie"
+    },
+    {
+        part: "Krup",
+        part2: "nik",
+        piwo: "Krupnik"
+    },
+    {
+        part: "Wybor",
+        part2: "owa",
+        piwo: "Wyborowa"
+    },
+    {
+        part: "Sas",
+        part2: "ka",
+        piwo: "Saska"
+    },
+    {
+        part: "Esk",
+        part2: "imos",
+        piwo: "Eskimos"
+    },
+    {
+        part: "Hus",
+        part2: "aria",
+        piwo: "Husaria"
+    },
+    {
+        part: "Absol",
+        part2: "ut",
+        piwo: "Absolut"
+    },
+    {
+        part: "Jager",
+        part2: "meister",
+        piwo: "Jagermeister"
+    },
+    {
+        part: "Mart",
+        part2: "ini",
+        piwo: "Martini"
+    },
+    {
+        part: "Gr",
+        part2: "ant's",
+        piwo: "Grant's"
+    },
+    {
+        part: "St",
+        part2: "ock",
+        piwo: "Stock"
+    },
+    {
+        part: "Łom",
+        part2: "ża",
+        piwo: "Łomża"
+    },
+];
+//captainJack lesnyDzban jackDaniels winoMarkiWino mocnaBeczka mocnyGrzmot cinCin cydrLubelski
+
+//eb piast
